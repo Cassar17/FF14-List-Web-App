fc0fa410123b22226c1ed6831d0020c90bd5ef3f
Cassar17 <lucascassar@gmail.com>
2024-09-29 18:30:38 -0300

    trying to fix gv.index

--- global_vars.py
+++ global_vars.py
@@ -11,6 +11,6 @@ hide_titles = True
 current_patch_index = 0
 
 arr_pages_list = ["A Realm Reborn (2.0)", "A Realm Awoken (2.1)", "Through the Maelstrom (2.2)", "Defenders of Eorzea (2.3)", "Dreams of Ice (2.4)", "Before the Fall Part 1 (2.5)", "Before the Fall Part 2 (2.55)"]
-hvs_pages_list = ["Heavensward (3.0)", "As Goes Light, So Goes Darkness (3.1)", "The Gears of Change (3.2)", "Revenge of the Horde (3.3)", "Soul Surrender (3.4)", "The Far Edge of Fate Part 1 (2.5)", "The Far Edge of Fate Part 2 (2.55)"]
+hvs_pages_list = ["Heavensward (3.0)", "As Goes Light, So Goes Darkness (3.1)", "The Gears of Change (3.2)", "Revenge of the Horde (3.3)", "Soul Surrender (3.4)", "The Far Edge of Fate Part 1 (3.5)", "The Far Edge of Fate Part 2 (3.56)"]
 
 pages_list = arr_pages_list + hvs_pages_list
